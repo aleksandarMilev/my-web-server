207834e808145d25b230087ca353ee55ab87d0a1
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-01-04 12:48:07 +0200

    Create route table

--- MyWebServer.HTTP/HttpServer.cs
+++ MyWebServer.HTTP/HttpServer.cs
@@ -1,10 +1,22 @@
 ﻿namespace MyWebServer.HTTP
 {
     using System;
+    using System.Collections.Generic;
 
     public class HttpServer : IHttpServer
     {
-        public void AddRoute(string path, Func<IHttpRequest, IHttpResponse> action) => throw new System.NotImplementedException();
+        private readonly Dictionary<string, Func<IHttpRequest, IHttpResponse>> routeTable = [];
+
+        public void AddRoute(string path, Func<IHttpRequest, IHttpResponse> action)
+        {
+            if (this.routeTable.ContainsKey(path))
+            {
+                this.routeTable[path] = action;
+                return;
+            }
+
+            this.routeTable.Add(path, action);
+        }
 
         public void Start(int port) => throw new System.NotImplementedException();
     }
